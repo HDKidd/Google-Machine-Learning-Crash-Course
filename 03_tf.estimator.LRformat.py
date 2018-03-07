42d7b6f68a84969e9b16b52c6562da639e5b844b
Kidd <HEDE0001@E.NTU.EDU.SG>
2018-03-07 11:31:54 +0800

    Add files via upload

---
+++ 03_tf.estimator.LRformat.py
@@ -0,0 +1,18 @@
+# !usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""TensorFlow"""
+
+__author__ = 'DKidd'
+
+import tensorflow as tf
+
+# Set up a linear classifier
+classifier = tf.estimator.LinearClassifier()
+
+# Train the model on some example data
+classifier.train(input_fn=train_input_fn, steps=2000)
+
+# Use it to predict
+predictions = classifier.predict(input_fn=predict_input_fn)
+
